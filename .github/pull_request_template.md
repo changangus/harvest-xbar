e8baad1632039a03d83a16659a513ee131bf2b25
Angus Chang <angus@heysparkbox.com>
2021-09-10 12:32:31 -0400

    chore: created CI workflow and PR template

---
+++ .github/pull_request_template.md
@@ -0,0 +1,16 @@
+## Description
+<!-- Add description of work done here -->
+
+## Spec
+
+See Issue: [ISSUE_NUMBER](https://sparkbox.atlassian.net/browse/FSA21V2-ISSUE_NUMBER)
+
+## Validation
+<!-- delete anything irrelevant to this PR -->
+
+- [ ] This PR has new code, so new tests were added or updated, and they pass.
+
+### To Validate
+
+1. Make sure all PR Checks have passed
+2. Make sure you can run the tests locally and all pass.
--- src/scripts/startTimer.sh
+++ src/scripts/startTimer.sh
@@ -1,7 +1,6 @@
 curl "https://api.harvestapp.com/v2/time_entries" \
   -H "Authorization: Bearer $ACCESS_TOKEN" \
   -H "Harvest-Account-Id: $ACCOUNT_ID" \
-  -H "User-Agent: MyApp (yourname@example.com)" \
   -X POST \
   -H "Content-Type: application/json" \
-  -d '{"user_id": $USER_ID,"project_id": $PROJECT_ID,"task_id": $TASK_ID,"spent_date":$(date '+%Y-%m-%d')}'
+  -d "{'user_id': $USER_ID,'project_id': $PROJECT_ID,'task_id': $TASK_ID,'spent_date': $TODAY}"
