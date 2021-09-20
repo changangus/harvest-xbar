76ffa99ed6789b31ed9af2ce16138a3f6c381533
Angus Chang <angus@heysparkbox.com>
2021-09-20 09:18:52 -0400

    fix: fixed env variable issue & tests
    env variables are now available in startTimer.js
    Tests now pass after changing the date format
    got rid of our dev test console log in harvest.1s.js
    which ran our openDirectory.sh file. Refactored our
    startTimer.sh to see if we can get it working and use
    those scripts instead of our js files to start, stop and
    restart timers in the future.

--- src/scripts/startTimer.sh
+++ src/scripts/startTimer.sh
@@ -3,5 +3,4 @@ curl "https://api.harvestapp.com/v2/time_entries" \
   -H "Harvest-Account-Id: $ACCOUNT_ID" \
   -X POST \
   -H "Content-Type: application/json" \
-  -d '{"user_id": 3855900, "project_id": '$PROJECT_ID',"task_id": '$TASK_ID',"spent_date":'$(date '%Y-%m-%d')'}'
-  
+  -d "{'user_id': $USER_ID,'project_id': $PROJECT_ID,'task_id': $TASK_ID,'spent_date': $TODAY}"
